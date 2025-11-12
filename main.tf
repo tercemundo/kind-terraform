d7f67419f5643e2a39d13c45acb29cb5f87da900
tercemundo <94326261+tercemundo@users.noreply.github.com>
2025-11-12 13:17:28 -0300

    Add Terraform configuration for Kind cluster

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "kind_cluster" "default" {
+  name            = "my-cluster"
+  wait_for_ready  = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+      
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+      }
+      
+      extra_port_mappings {
+        container_port = 443
+        host_port      = 443
+      }
+    }
+
+    node {
+      role = "worker"
+    }
+
+    node {
+      role = "worker"
+    }
+
+   node {
+      role = "worker"
+    }
+  }
+}
